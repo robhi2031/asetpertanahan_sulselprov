5d9ea207ac19f44ba2afe03e9e5d927b790fb372
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-07-27 05:05:07 +0800

    Create 2023_07_25_000000_create_site_info_table.php

---
+++ database/migrations/2023_07_25_000000_create_site_info_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('short_name');
+            $table->string('description');
+            $table->text('keyword');
+            // $table->text('thumb');
+            $table->text('login_bg');
+            $table->text('login_logo');
+            $table->text('frontend_logo');
+            $table->text('backend_logo');
+            $table->text('backend_logo_icon');
+            $table->text('copyright');
+            $table->text('iframe_map')->nullable();
+            $table->integer('user_updated')->nullable();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_info');
+    }
+};
