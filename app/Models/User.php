a2042be5a3c21757feba7762554fe2c12c26d294
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-07-27 05:04:58 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,18 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\NewAccessToken;
+use App\Traits\HasPermissionsTrait;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasPermissionsTrait;
+
+    protected $table = 'users_system';
+
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
@@ -19,8 +26,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
-        'password',
+        'phone_number',
+        'thumb',
+        'is_active',
+        'user_add',
+        'user_updated',
+        'created_at',
+        'updated_at',
     ];
 
     /**
@@ -41,4 +55,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * createToken
+     *
+     * @param  mixed $name
+     * @param  mixed $abilities
+     * @return void
+     */
+    public function createToken(string $name, array $abilities = ['*']) {
+        $token = $this->tokens()->create([
+            'name' => $name,
+            'token' => hash('sha256', $plainTextToken = Str::random(250)),
+            'abilities' => $abilities,
+        ]);
+
+        return new NewAccessToken($token, $token->getKey().'|'.$plainTextToken);
+    }
 }
