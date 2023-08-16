6854069045187412c04c528fb7f032f74fec5123
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-17 01:41:44 +0800

    Update scripts.blade.php

--- resources/views/backend/partials/scripts.blade.php
+++ resources/views/backend/partials/scripts.blade.php
@@ -5,8 +5,6 @@
 <!--begin::Global Javascript Bundle(mandatory for all pages)-->
 <script src="{{ asset('/dist/plugins/global/plugins.bundle.js') }}"></script>
 <script src="{{ asset('/dist/js/scripts.bundle.js') }}"></script>
-<script src="{{ asset('/dist/plugins/angular-1.2.13/angular.min.js') }}"></script>
-<script src="{{ asset('/dist/plugins/AngularUI/ui-router.min.js') }}"></script>
 <!--end::Global Javascript Bundle-->
 <!--begin::Vendors Javascript(used for this page only)-->
 @foreach ($data['js'] as $dt)
