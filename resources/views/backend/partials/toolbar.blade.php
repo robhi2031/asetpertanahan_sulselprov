32b7fd6d12814082009c7154633cd334581f207c
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-17 01:41:46 +0800

    Update toolbar.blade.php

--- resources/views/backend/partials/toolbar.blade.php
+++ resources/views/backend/partials/toolbar.blade.php
@@ -1,38 +1,40 @@
 <!--begin::Toolbar-->
-<div id="kt_app_toolbar" class="app-toolbar py-6">
+<div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6" style="display: none;">
     <!--begin::Toolbar container-->
-    <div id="kt_app_toolbar_container"
-        class="app-container  container-xxl d-flex align-items-start">
-        <!--begin::Toolbar container-->
-        <div class="d-flex flex-column flex-row-fluid">
-            <!--begin::Toolbar wrapper-->
-            <div class="d-flex align-items-center pt-1">
-                <!--begin::Breadcrumb-->
-                <ul class="breadcrumb breadcrumb-separatorless fw-semibold">
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-white fw-bold lh-1">
-                        <a href="{{ url('/app_admin/') }}" class="text-white text-hover-warning">
-                            <i class="ki-outline ki-home text-white fs-3"></i>
-                        </a>
-                    </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item">
-                        <i class="ki-outline ki-right fs-4 text-white mx-n1"></i>
-                    </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-white fw-bold lh-1" id="item-breadCrumb"></li>
-                    <!--end::Item-->
-                </ul>
-                <!--end::Breadcrumb-->
-            </div>
-            <!--end::Toolbar wrapper--->
-            <!--begin::Toolbar wrapper--->
-            <div class="d-flex flex-stack flex-wrap flex-lg-nowrap gap-4 gap-lg-10 pt-6 pb-18 py-lg-13" id="pageTitle"></div>
-            <!--end::Toolbar wrapper--->
+    <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
+        <!--begin::Page title-->
+        <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
+            <h1
+                class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
+                Licenses
+            </h1>
+            <!--begin::Breadcrumb-->
+            <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                <li class="breadcrumb-item text-muted">
+                    <a href="/metronic8/demo1/../demo1/index.html"
+                        class="text-muted text-hover-primary">
+                        Home </a>
+                </li>
+                <li class="breadcrumb-item">
+                    <span class="bullet bg-gray-400 w-5px h-2px"></span>
+                </li>
+                <li class="breadcrumb-item text-muted">
+                    Corporate
+                </li>
+            </ul>
+            <!--end::Breadcrumb-->
         </div>
-        <!--end::Toolbar container--->
+        <!--end::Page title-->
+        <!--begin::Actions-->
+        <div class="d-flex align-items-center gap-2 gap-lg-3">
+            <!--begin::Primary button-->
+            <a href="#" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
+                data-bs-target="#kt_modal_create_app">
+                Create
+            </a>
+            <!--end::Primary button-->
+        </div>
+        <!--end::Actions-->
     </div>
     <!--end::Toolbar container-->
 </div>
