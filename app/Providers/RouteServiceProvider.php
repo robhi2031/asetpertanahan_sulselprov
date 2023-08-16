8b04d6626df1fb46f2e13944b7e6d45d8592acce
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-16 23:37:58 +0800

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/app_admin/';
 
     /**
      * The controller namespace for the application.
