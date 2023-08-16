bc6ab84747b79deb2314a3857a690e3c108effc7
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-16 23:37:59 +0800

    Update SiteCommon.php

--- app/Traits/SiteCommon.php
+++ app/Traits/SiteCommon.php
@@ -15,16 +15,16 @@ trait SiteCommon {
     {
         $getRow = SiteInfo::where('id', 1)->first();
         if($getRow==true){
-            //Frontend Logo Site
-            $frontend_logo = $getRow->frontend_logo;
-            if($frontend_logo==''){
+            //Head Logo Site
+            $head_logo = $getRow->head_logo;
+            if($head_logo==''){
                 $getRow->url_frontendLogo = NULL;
             } else {
-                if (!file_exists(public_path(). '/dist/img/site-img/'.$frontend_logo)){
-                    $getRow->url_frontendLogo = NULL;
-                    $getRow->frontend_logo = NULL;
+                if (!file_exists(public_path(). '/dist/img/site/'.$head_logo)){
+                    $getRow->url_headLogo = NULL;
+                    $getRow->head_logo = NULL;
                 }else{
-                    $getRow->url_frontendLogo = url('dist/img/site-img/'.$frontend_logo);
+                    $getRow->url_headLogo = url('dist/img/site/'.$head_logo);
                 }
             }
             //Login Background
@@ -32,11 +32,11 @@ trait SiteCommon {
             if($login_bg==''){
                 $getRow->url_loginBg = NULL;
             } else {
-                if (!file_exists(public_path(). '/dist/img/site-img/'.$login_bg)){
+                if (!file_exists(public_path(). '/dist/img/site/'.$login_bg)){
                     $getRow->url_loginBg = NULL;
                     $getRow->login_bg = NULL;
                 }else{
-                    $getRow->url_loginBg = url('dist/img/site-img/'.$login_bg);
+                    $getRow->url_loginBg = url('dist/img/site/'.$login_bg);
                 }
             }
             //Login Logo
@@ -44,35 +44,35 @@ trait SiteCommon {
             if($login_logo==''){
                 $getRow->url_loginLogo = NULL;
             } else {
-                if (!file_exists(public_path(). '/dist/img/site-img/'.$login_logo)){
+                if (!file_exists(public_path(). '/dist/img/site/'.$login_logo)){
                     $getRow->url_loginLogo = NULL;
                     $getRow->login_logo = NULL;
                 }else{
-                    $getRow->url_loginLogo = url('dist/img/site-img/'.$login_logo);
+                    $getRow->url_loginLogo = url('dist/img/site/'.$login_logo);
                 }
             }
-            //Backend Logo
-            $backend_logo = $getRow->backend_logo;
-            if($backend_logo==''){
-                $getRow->url_backendLogo = NULL;
+            //Head Logo Dark Site
+            $head_logo_dark = $getRow->head_logo_dark;
+            if($head_logo_dark==''){
+                $getRow->url_frontendLogo = NULL;
             } else {
-                if (!file_exists(public_path(). '/dist/img/site-img/'.$backend_logo)){
-                    $getRow->url_backendLogo = NULL;
-                    $getRow->backend_logo = NULL;
+                if (!file_exists(public_path(). '/dist/img/site/'.$head_logo_dark)){
+                    $getRow->url_headLogoDark = NULL;
+                    $getRow->head_logo_dark = NULL;
                 }else{
-                    $getRow->url_backendLogo = url('dist/img/site-img/'.$backend_logo);
+                    $getRow->url_headLogoDark = url('dist/img/site/'.$head_logo_dark);
                 }
             }
-            //Backend Logo Icon
-            $backend_logo_icon = $getRow->backend_logo_icon;
-            if($backend_logo_icon==''){
-                $getRow->url_backendLogoIcon = NULL;
+            //Head Logo Small
+            $head_logo_small = $getRow->head_logo_small;
+            if($head_logo_small==''){
+                $getRow->url_headLogoSmall = NULL;
             } else {
-                if (!file_exists(public_path(). '/dist/img/site-img/'.$backend_logo_icon)){
-                    $getRow->url_backendLogoIcon = NULL;
-                    $getRow->backend_logo_icon = NULL;
+                if (!file_exists(public_path(). '/dist/img/site/'.$head_logo_small)){
+                    $getRow->url_headLogoSmall = NULL;
+                    $getRow->head_logo_small = NULL;
                 }else{
-                    $getRow->url_backendLogoIcon = url('dist/img/site-img/'.$backend_logo_icon);
+                    $getRow->url_headLogoSmall = url('dist/img/site/'.$head_logo_small);
                 }
             }
             //Keyword to Explode
