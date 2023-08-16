23edb1addf9e08ae29ab38e704c5cfe0cffbbad7
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-17 01:41:40 +0800

    Update layouts.blade.php

--- resources/views/backend/layouts.blade.php
+++ resources/views/backend/layouts.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<base href="{{ url('/') }}">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -10,16 +9,11 @@
     <title>
         {{ isset($data['title']) ? $data['title'] : 'Unknown'; }}
     </title>
-    <meta name="description" content="{{ $data['desc'] }}" />
-    <meta name="keywords" content="{{ $data['keywords'] }}" />
     <meta name="author" content="@RobhiTranzad" />
     <meta name="email" content="robhi.sanjaya@gmail.com" />
     <meta name="website" content="{{ $data['url'] }}" />
     <meta name="Version" content="{{ $data['app_version'] }}" />
-    <meta name="docsearch:language" content="id">
-    <meta name="docsearch:version" content="{{ $data['app_version'] }}" />
-    <link rel="canonical" href="{{ $data['url'] }}" />
-    <!-- Favicons -->
+    <!-- start::Favicons -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/dist/img/favicon/apple-touch-icon.png') }}" />
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/dist/img/favicon/favicon-32x32.png') }}" />
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/dist/img/favicon/favicon-16x16.png') }}" />
@@ -28,35 +22,18 @@
     <meta name="msapplication-TileColor" content="#061824" />
     <meta name="theme-color" content="#D99400" />
     <meta name="application-name" content="{{ $data['app_name'] }}" />
-    <meta name="msapplication-TileImage" content="{{ $data['thumb'] }}" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="HandheldFriendly" content="true" />
-    <!-- Twitter -->
-    <meta name="twitter:widgets:csp" content="on" />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:url" content="{{ $data['url'] }}" />
-    <meta name="twitter:site" content="{{ $data['app_name'] }}" />
-    <meta name="twitter:creator" content="@robhitranzad" />
-    <meta name="twitter:title" content="{{ $data['title'] }}" />
-    <meta name="twitter:description" content="{{ $data['desc'] }}" />
-    <meta name="twitter:image" content="{{ $data['thumb'] }}" />
-    <!-- Facebook -->
-    <meta property="og:locale" content="id_ID" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="{{ $data['url'] }}" />
-    <meta property="og:title" content="{{ $data['title'] }}" />
-    <meta property="og:description" content="{{ $data['desc'] }}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:image" content="{{ $data['thumb'] }}" />
-    <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1000" />
-    <meta property="og:image:height" content="500" />
+    <!-- end::Favicons -->
     @include('backend.partials.styles')
 </head>
 <!--end::Head-->
 <!--begin::Body-->
-<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true" class="app-default" ng-app="routerApp">
+<body id="kt_app_body" data-kt-app-layout="dark-sidebar" data-kt-app-header-fixed="true"
+    data-kt-app-header-fixed-mobile="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true"
+    data-kt-app-sidebar-hoverable="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true"
+    data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
     <!--begin::Theme mode setup on page load-->
     <script>
         var defaultThemeMode = "light";
@@ -81,24 +58,30 @@
     <!--begin::App-->
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
         <!--begin::Page-->
-        <div class="app-page  flex-column flex-column-fluid" id="kt_app_page">
+        <div class="app-page flex-column flex-column-fluid" id="kt_app_page">
             @include('backend.partials.header')
             <!--begin::Wrapper-->
-            <div class="app-wrapper  flex-column flex-row-fluid" id="kt_app_wrapper">
-                @include('backend.partials.toolbar')
-                <!--begin::Wrapper container-->
-                <div class="app-container container-xxl">
-                    <!--begin::Main-->
-                    <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
-                        <!-- Body Content start -->
-                        {{-- @yield('content') --}}
-                        <div ui-view></div>
-                        <!-- Body Content end -->
-                        @include('backend.partials.footer')
+            <div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+                @include('backend.partials.sidebar')
+                <!--begin::Main-->
+                <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+                    <!--begin::Content wrapper-->
+                    <div class="d-flex flex-column flex-column-fluid">
+                        @include('backend.partials.toolbar')
+                        <!--begin::Content-->
+                        <div id="kt_app_content" class="app-content flex-column-fluid">
+                            <!--begin::Content container-->
+                            <div id="kt_app_content_container" class="app-container container-xxl">
+                                @yield('content')
+                            </div>
+                            <!--end::Content container-->
+                        </div>
+                        <!--end::Content-->
                     </div>
-                    <!--end:::Main-->
+                    <!--end::Content wrapper-->
+                    @include('backend.partials.footer')
                 </div>
-                <!--end::Wrapper container-->
+                <!--end:::Main-->
             </div>
             <!--end::Wrapper-->
         </div>
