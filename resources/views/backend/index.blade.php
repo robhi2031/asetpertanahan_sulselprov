1a0a137ba2715ef17d18b21fe629c5ea399a8742
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-17 01:41:39 +0800

    Update index.blade.php

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,12 +1,126 @@
-{{-- @extends('backend.layouts', ['activeMenu' => 'Beranda', 'activeSubMenu' => '']) --}}
-{{-- @section('content') --}}
-<!--begin::Content wrapper-->
-<div class="d-flex flex-column flex-column-fluid">
-    <!--begin::Content-->
-    <div id="kt_app_content" class="app-content">
+@extends('backend.layouts', ['activeMenu' => 'DASHBOARD', 'activeSubMenu' => ''])
+@section('content')
+<!--begin::card-->
+<div class="card my-3 my-lg-6">
+    <!--begin::Body-->
+    <div class="card-body">
+        Test
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
+        <!--begin::Text-->
+        <div class="fs-5 text-gray-600 fw-semibold">
+            First, a disclaimer – the entire process of writing a blog post often takes more than a couple of hours, even if you can type 
+            eighty words as per minute and your writing skills are sharp.    
+        </div>   
+        <!--end::Text-->
     </div>
-    <!--end::Content-->
+    <!--end::Body-->
 </div>
-<!--end::Content wrapper-->
-{{-- @endsection --}}
-<script src="{{ asset('/dist/scripts/backend/main.init.js') }}"></script>
+<!--end::card-->
+@endsection
