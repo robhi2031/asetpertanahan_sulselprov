23642c0f34daf426d1e813edd09aafe06cde9d2e
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-17 01:41:42 +0800

    Update header.blade.php

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -1,35 +1,29 @@
 <!--begin::Header-->
 <div id="kt_app_header" class="app-header shadow" data-kt-sticky="true"
-    data-kt-sticky-activate="{default: false, lg: true}" data-kt-sticky-name="app-header-sticky"
-    data-kt-sticky-offset="{default: false, lg: '300px'}">
+    data-kt-sticky-activate="{default: true, lg: true}" data-kt-sticky-name="app-header-minimize"
+    data-kt-sticky-offset="{default: '200px', lg: '0'}" data-kt-sticky-animation="false">
     <!--begin::Header container-->
-    <div class="app-container  container-xxl d-flex align-items-stretch justify-content-between " id="kt_app_header_container">
-        <!--begin::Header mobile toggle-->
-        <div class="d-flex align-items-center d-lg-none ms-n3 me-2" title="Show sidebar menu">
-            <div class="btn btn-icon btn-color-gray-600 btn-active-color-primary w-35px h-35px"
-                id="kt_app_header_menu_toggle">
-                <i class="ki-outline ki-abstract-14 fs-2"></i>
+    <div class="app-container container-fluid d-flex align-items-stretch justify-content-between" id="kt_app_header_container">
+        <!--begin::Sidebar mobile toggle-->
+        <div class="d-flex align-items-center d-lg-none ms-n3 me-1 me-md-2" title="Show sidebar menu">
+            <div class="btn btn-icon btn-active-color-primary w-35px h-35px" id="kt_app_sidebar_mobile_toggle">
+                <i class="ki-outline ki-abstract-14 fs-2 fs-md-1"></i>
             </div>
         </div>
-        <!--end::Header mobile toggle-->
-        <!--begin::Logo-->
-        <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0 me-lg-15" id="headerLogo">
-            <a href="{{ url('/') }}">
-                <svg class="bd-placeholder-img rounded w-100 h-35px d-lg-none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="#868e96"></rect>
-                </svg>
-                <svg class="bd-placeholder-img rounded w-100 h-45px d-none d-lg-inline app-sidebar-logo-default theme-light-show" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="#868e96"></rect>
-                </svg>
-                <svg class="bd-placeholder-img rounded w-100 h-45px d-none d-lg-inline app-sidebar-logo-default theme-dark-show" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <!--end::Sidebar mobile toggle-->
+        <!--begin::Mobile logo-->
+        <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
+            <a href="{{ url('/app_admin') }}" class="d-lg-none mobileHeaderLogo">
+                <svg class="bd-placeholder-img rounded w-50 h-30px" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <rect width="100%" height="100%" fill="#868e96"></rect>
                 </svg>
             </a>
         </div>
-        <!--end::Logo-->
+        <!--end::Mobile logo-->
         <!--begin::Header wrapper-->
-        <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1" id="kt_app_header_wrapper">
-            @include('backend.partials.menus')
+        {{-- <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1" id="kt_app_header_wrapper"> --}}
+        <div class="d-flex align-items-stretch" id="kt_app_header_wrapper">
+            {{-- @include('backend.partials.menus') --}}
             @include('backend.partials.navbar')
         </div>
         <!--end::Header wrapper-->
