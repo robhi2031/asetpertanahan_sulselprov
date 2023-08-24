00aadfa266038f15d4ac47bcd76bce289baff7cb
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-24 09:04:01 +0800

    Update toolbar.blade.php

--- resources/views/backend/partials/toolbar.blade.php
+++ resources/views/backend/partials/toolbar.blade.php
@@ -1,41 +1,24 @@
-<!--begin::Toolbar-->
-<div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6" style="display: none;">
-    <!--begin::Toolbar container-->
-    <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
-        <!--begin::Page title-->
-        <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
-            <h1
-                class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
-                Licenses
-            </h1>
-            <!--begin::Breadcrumb-->
-            <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                <li class="breadcrumb-item text-muted">
-                    <a href="/metronic8/demo1/../demo1/index.html"
-                        class="text-muted text-hover-primary">
-                        Home </a>
-                </li>
-                <li class="breadcrumb-item">
-                    <span class="bullet bg-gray-400 w-5px h-2px"></span>
-                </li>
-                <li class="breadcrumb-item text-muted">
-                    Corporate
-                </li>
-            </ul>
-            <!--end::Breadcrumb-->
-        </div>
-        <!--end::Page title-->
-        <!--begin::Actions-->
-        <div class="d-flex align-items-center gap-2 gap-lg-3">
-            <!--begin::Primary button-->
-            <a href="#" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
-                data-bs-target="#kt_modal_create_app">
-                Create
+<!--begin::Page title-->
+<div data-kt-swapper="true" data-kt-swapper-mode="{default: 'prepend', lg: 'prepend'}"
+    data-kt-swapper-parent="{default: '#kt_app_content_container', lg: '#kt_app_header_wrapper'}"
+    class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+    <h1 class="page-heading d-flex text-dark fw-bold fs-3 align-items-center my-0">
+        {{ isset($data['title']) ? $data['title'] : 'Unknown'; }}
+    </h1>
+    <!--
+    <span class="h-20px border-gray-300 border-start mx-4"></span>
+    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 ">
+        <li class="breadcrumb-item text-muted">
+            <a href="/metronic8/demo1/../demo1/index.html" class="text-muted text-hover-primary">
+                Home
             </a>
-            <!--end::Primary button-->
-        </div>
-        <!--end::Actions-->
-    </div>
-    <!--end::Toolbar container-->
+        </li>
+        <li class="breadcrumb-item">
+            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+        </li>
+        <li class="breadcrumb-item text-muted">
+            Chat
+        </li>
+    </ul>-->
 </div>
-<!--end::Toolbar-->
+<!--end::Page title-->
