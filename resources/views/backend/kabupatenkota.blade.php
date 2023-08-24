1b3d2609cce59c1826422d403d75bdcca67704f4
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-24 09:03:51 +0800

    Create kabupatenkota.blade.php

---
+++ resources/views/backend/kabupatenkota.blade.php
@@ -0,0 +1,12 @@
+@extends('backend.layouts', ['activeMenu' => 'MASTER DATA', 'activeSubMenu' => 'Kabupaten/ Kota'])
+@section('content')
+<!--begin::card-->
+<div class="card my-3 my-lg-6">
+    <!--begin::Body-->
+    <div class="card-body">
+        
+    </div>
+    <!--end::Body-->
+</div>
+<!--end::card-->
+@endsection
